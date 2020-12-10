ad0c5dd0da82da0a4480b2cbc49c34d9a9f87c2c
stanleyoos <stanislaw.sarka@gmail.com>
2020-12-10 15:53:08 +0100

    Final changes

--- main.js
+++ main.js
@@ -22,4 +22,53 @@ function getResult(query) {
 
 function displayResult(weather) {
   console.log(weather);
+  let city = document.querySelector(".location .city");
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+  let now = new Date();
+  let date = document.querySelector(".location .date");
+  date.innerText = dateBuilder(now);
+
+  let temperature = document.querySelector(".current .temp");
+  temperature.innerText = Math.floor(weather.main.temp);
+
+  let info = document.querySelector(".current .weather");
+  info.innerText = weather.weather[0].main;
+
+  let hilow = document.querySelector(".current .hi-low");
+  hilow.innerText = `${Math.floor(weather.main.temp_max)} / ${Math.floor(
+    weather.main.temp_min
+  )}`;
+}
+
+function dateBuilder(d) {
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`;
 }
